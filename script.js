3df57d2e2b116a0db4d17a737bd9011c8c7af424
Simran <f20201445@goa.bits-pilani.ac.in>
2021-06-15 23:30:31 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// var a=prompt("Enter UserName");
+//  var b=prompt("Enter password");
+//   	if(a== "Shyam" && b=="25-06-2001")
+//   	{
+//   	    window.location.href="start.html";
+//   	}
+//   	else
+//   	{
+//   		alert("invalid details");
+//   		window.location.href="index.html";
+//   	}
+  	
